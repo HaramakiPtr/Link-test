a0a811677ae526038927a43d9250ceb785571e40
haramaki[ptr] <49096527+HaramakiPtr@users.noreply.github.com>
2019-03-30 23:34:57 +0900

    Update main.c

--- Source/main.c
+++ Source/main.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include "test.h"
 
-int main(voi)
+int main(void)
 {
-	printf("Please enter your name:");
-	char String[20];
-	scanf("%s",String);
-	char* result1 = returnMsg(String);
-	printf("%s",result1);
-	free(result1);
-	return 0;
+  printf("Please enter your name:");
+  char String[20];
+  scanf("%s",String);
+  char* result1 = returnMsg(String);
+  printf("%s",result1);
+  free(result1);
+  return 0;
 }
